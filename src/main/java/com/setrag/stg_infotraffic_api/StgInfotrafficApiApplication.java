dc7a6e1e34837d4320efbb1f0c103be684ca2a8f
bugtronik <bugtronik@gmail.com>
2025-07-03 17:25:36 +0100

    Setting up environment variables

--- src/main/java/com/setrag/stg_infotraffic_api/StgInfotrafficApiApplication.java
+++ src/main/java/com/setrag/stg_infotraffic_api/StgInfotrafficApiApplication.java
@@ -3,11 +3,18 @@ package com.setrag.stg_infotraffic_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class StgInfotrafficApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
 		SpringApplication.run(StgInfotrafficApiApplication.class, args);
+		
 	}
 
 }
