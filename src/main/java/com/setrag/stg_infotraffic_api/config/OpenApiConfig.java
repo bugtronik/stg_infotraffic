e94730110042486ebd958b5e9bbfe490e16a4e62
Aly <38461911+bugtronik@users.noreply.github.com>
2025-07-07 07:22:42 +0100

    Update OpenApiConfig.java

--- src/main/java/com/setrag/stg_infotraffic_api/config/OpenApiConfig.java
+++ src/main/java/com/setrag/stg_infotraffic_api/config/OpenApiConfig.java
@@ -5,8 +5,8 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement; // Importez cette annotation
-import io.swagger.v3.oas.annotations.security.SecurityScheme; // Importez cette annotation
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
